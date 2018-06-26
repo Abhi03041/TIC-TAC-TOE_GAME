754e8819e748609a7b13e2183c0d8f71d30c4f4c
Abhinav Mittal <35320556+Abhi03041@users.noreply.github.com>
2018-06-26 21:59:12 +0530

    Update tictocgame.py

--- tictocgame.py
+++ tictocgame.py
@@ -1,42 +1,60 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jun 26 12:06:03 2018
+
+@author: ABHINAVMITTAL
+"""
+
 from tkinter import*
 from tkinter import messagebox
 
-win =Tk()    # figure  k is small  
-win.title('First_GUI') #title
-win.grid()  #geometry
+home =Tk()    # figure  k is small
+home.geometry('670x455')
+home.title('Tic_Toc_Game') #title
+
+top=Frame(home)
+top.grid()
+
+win=Frame(home)
+win.grid()
 
 click_number = IntVar()
+click_number.set(0)
 turnnumber = IntVar(0)
-one = IntVar(0)
-one.set(-1)
-two = IntVar(0)
-two.set(-1)
-three = IntVar(0)
-three.set(-1)
-four = IntVar(0)
-four.set(-1)
-five = IntVar(0)
-five.set(-1)
-six = IntVar(0)
-six.set(-1)
-seven = IntVar(0)
-seven.set(-1)
-eight = IntVar(0)
-eight.set(-1)
-nine = IntVar(0)
-nine.set(-1)
+
+m=[-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1]
 
 def displaymessage(mess):
     messagebox.showinfo("Game_Result",mess)
+    m.clear()
+    for i in range(12):
+        m.append(-1)
+    reset()
 
-
+def reset():
+    click_number.set(0)
+    turnnumber.set(0)
+    firstplayer.set('')
+    secondplayer.set('')
+    button1.config(state='active',text='')
+    button2.config(state='active',text='')
+    button3.config(state='active',text='')
+    button4.config(state='active',text='')
+    button5.config(state='active',text='')
+    button6.config(state='active',text='')
+    button7.config(state='active',text='')
+    button8.config(state='active',text='')
+    button9.config(state='active',text='')
+    mess=''
+    
 def click(a):
     d=click_number.get()
+    #print(d)
     d=d+1
     click_number.set(d)
     if(a==1):
         d=turnnumber.get()
-        one.set(d)
+        m[a]=d
         if(d==0):
             button1['text']='0'
             d=1
@@ -47,7 +65,7 @@ def click(a):
         button1.config(state="disabled")
     elif(a==2):
         d=turnnumber.get()
-        two.set(d)
+        m[a]=d
         if(d==0):
             button2['text']='0'
             d=1
@@ -58,7 +76,7 @@ def click(a):
         button2.config(state="disabled")
     elif(a==3):
         d=turnnumber.get()
-        three.set(d)
+        m[a]=d
         if(d==0):
             button3['text']='0'
             d=1
@@ -69,7 +87,7 @@ def click(a):
         button3.config(state="disabled")
     elif(a==4):
         d=turnnumber.get()
-        four.set(d)
+        m[a]=d
         if(d==0):
             button4['text']='0'
             d=1
@@ -80,7 +98,7 @@ def click(a):
         button4.config(state="disabled")
     elif(a==5):
         d=turnnumber.get()
-        five.set(d)
+        m[a]=d
         if(d==0):
             button5['text']='0'
             d=1
@@ -91,7 +109,7 @@ def click(a):
         button5.config(state="disabled")
     elif(a==6):
         d=turnnumber.get()
-        six.set(d)
+        m[a]=d
         if(d==0):
             button6['text']='0'
             d=1
@@ -102,7 +120,7 @@ def click(a):
         button6.config(state="disabled")
     elif(a==7):
         d=turnnumber.get()
-        seven.set(d)
+        m[a]=d
         if(d==0):
             button7['text']='0'
             d=1
@@ -113,7 +131,7 @@ def click(a):
         button7.config(state="disabled")
     elif(a==8):
         d=turnnumber.get()
-        eight.set(d)
+        m[a]=d
         if(d==0):
             button8['text']='0'
             d=1
@@ -124,7 +142,7 @@ def click(a):
         button8.config(state="disabled")
     elif(a==9):
         d=turnnumber.get()
-        nine.set(d)
+        m[a]=d
         if(d==0):
             button9['text']='0'
             d=1
@@ -134,53 +152,63 @@ def click(a):
         turnnumber.set(d)
         button9.config(state="disabled")
     if(click_number.get() < 9 ):  
-        if((one.get()==1 and two.get()==1 and three.get()==1) or (one.get()==1 and four.get()==1 and seven.get()==1) or (one.get()==1 and five.get()==1 and nine.get()==1) or (four.get()==1 and five.get()==1 and six.get()==1) or (seven.get()==1 and eight.get()==1 and nine.get()==1) or (two.get()==1 and five.get()==1 and eight.get()==1) or (three.get()==1 and six.get()==1 and nine.get()==1) or (three.get()==1 and five.get()==1 and seven.get()==1)):
-            mess='Winner is 1'
+        if((m[1]==1 and m[2]==1 and m[3]==1) or (m[1]==1 and m[4]==1 and m[7]==1) or (m[1]==1 and m[5]==1 and m[9]==1) or (m[4]==1 and m[5]==1 and m[6]==1) or (m[7]==1 and m[8]==1 and m[9]==1) or (m[2]==1 and m[5]==1 and m[8]==1) or (m[3]==1 and m[6]==1 and m[9]==1) or (m[3]==1 and m[5]==1 and m[7]==1)):
+            mess='Winner is %s'%(secondplayer.get())
             displaymessage(mess)
-            click_number.set(10)
-        if((one.get()==0 and two.get()==0 and three.get()==0) or (one.get()==0 and four.get()==0 and seven.get()==0) or (one.get()==0 and five.get()==0 and nine.get()==0) or (four.get()==0 and five.get()==0 and six.get()==0) or (seven.get()==0 and eight.get()==0 and nine.get()==0) or (two.get()==0 and five.get()==0 and eight.get()==0) or (three.get()==0 and six.get()==0 and nine.get()==0) or (three.get()==0 and five.get()==0 and seven.get()==0)):
-            mess='Winner is 0'
+        elif((m[1]==0 and m[2]==0 and m[3]==0) or (m[1]==0 and m[4]==0 and m[7]==0) or (m[1]==0 and m[5]==0 and m[9]==0) or (m[4]==0 and m[5]==0 and m[6]==0) or (m[7]==0 and m[8]==0 and m[9]==0) or (m[2]==0 and m[5]==0 and m[8]==0) or (m[3]==0 and m[6]==0 and m[9]==0) or (m[3]==0 and m[5]==0 and m[7]==0)):
+            mess='Winner is %s'%(firstplayer.get())
             displaymessage(mess)
-            click_number.set(10)
     if(click_number.get() == 9):
-        if((one.get()==1 and two.get()==1 and three.get()==1) or (one.get()==1 and four.get()==1 and seven.get()==1) or (one.get()==1 and five.get()==1 and nine.get()==1) or (four.get()==1 and five.get()==1 and six.get()==1) or (seven.get()==1 and eight.get()==1 and nine.get()==1) or (two.get()==1 and five.get()==1 and eight.get()==1) or (three.get()==1 and six.get()==1 and nine.get()==1) or (three.get()==1 and five.get()==1 and seven.get()==1)):
-            mess='Winner is 1'
+        if((m[1]==1 and m[2]==1 and m[3]==1) or (m[1]==1 and m[4]==1 and m[7]==1) or (m[1]==1 and m[5]==1 and m[9]==1) or (m[4]==1 and m[5]==1 and m[6]==1) or (m[7]==1 and m[8]==1 and m[9]==1) or (m[2]==1 and m[5]==1 and m[8]==1) or (m[3]==1 and m[6]==1 and m[9]==1) or (m[3]==1 and m[5]==1 and m[7]==1)):
+            mess='Winner is %s'%(secondpalyer.get())
             displaymessage(mess)
-            click_number.set(10)
-        elif((one.get()==0 and two.get()==0 and three.get()==0) or (one.get()==0 and four.get()==0 and seven.get()==0) or (one.get()==0 and five.get()==0 and nine.get()==0) or (four.get()==0 and five.get()==0 and six.get()==0) or (seven.get()==0 and eight.get()==0 and nine.get()==0) or (two.get()==0 and five.get()==0 and eight.get()==0) or (three.get()==0 and six.get()==0 and nine.get()==0) or (three.get()==0 and five.get()==0 and seven.get()==0)):
-            mess='Winner is 0'
+        elif((m[1]==0 and m[2]==0 and m[3]==0) or (m[1]==0 and m[4]==0 and m[7]==0) or (m[1]==0 and m[5]==0 and m[9]==0) or (m[4]==0 and m[5]==0 and m[6]==0) or (m[7]==0 and m[8]==0 and m[9]==0) or (m[2]==0 and m[5]==0 and m[8]==0) or (m[3]==0 and m[6]==0 and m[9]==0) or (m[3]==0 and m[5]==0 and m[7]==0)):
+            mess='Winner is %s'%(firstplayer.get())
             displaymessage(mess)
-            click_number.set(10)
         else:
             mess='Draw'
             displaymessage(mess)
-            click_number.set(10)
+    #print(m)
+firstplayer=StringVar()
+secondplayer=StringVar()
 
-button1=Button(win,text='',font = ("Arial", 20, "bold"), height=3,width=12,fg='green',bg='pink',command=lambda :click(1))   #name of figure,text on button,height=no. of lines,width=no. of char,fg=color of text,bg=background color,command=to perform an action after clicking on botton
-button1.grid(row=1,column=0)
+firstplayer1=Label(top,text='First Player:',font = ("Arial", 20, "bold"),fg ='blue' )
+firstplayer1.grid(row=1,column=0)
 
-button2=Button(win,text='',font = ("Arial", 20, "bold"), height=3,width=12,fg='green',bg='pink',command=lambda :click(2))
-button2.grid(row=1,column=1)
+firstplayerEntry=Entry(top,textvariable=firstplayer,font = ("Arial", 15, "bold"))
+firstplayerEntry.grid(row=1,column=1)
 
-button3=Button(win,text='',font = ("Arial", 20, "bold"), height=3,width=12,fg='green',bg='pink',command=lambda :click(3))
-button3.grid(row=1,column=2)
+secondplayer2=Label(top,text='Second Player:',font = ("Arial", 20, "bold"),fg='red')
+secondplayer2.grid(row=2,column=0)
 
-button4=Button(win,text='',font = ("Arial", 20, "bold"), height=3,width=12,fg='green',bg='pink',command=lambda :click(4))   #name of figure,text on button,height=no. of lines,width=no. of char,fg=color of text,bg=background color,command=to perform an action after clicking on botton
-button4.grid(row=2,column=0)
+secondplayerEntry=Entry(top,textvariable=secondplayer,font = ("Arial", 15, "bold"))
+secondplayerEntry.grid(row=2,column=1)
 
-button5=Button(win,text='',font = ("Arial", 20, "bold"), height=3,width=12,fg='green',bg='pink',command=lambda :click(5))
-button5.grid(row=2,column=1)
+button1=Button(win,text='',font = ("Arial", 20, "bold"), height=3,width=12,bd=5,fg='green',command=lambda :click(1))   #name of figure,text on button,height=no. of lines,width=no. of char,fg=color of text,bg=background color,command=to perform an action after clicking on button
+button1.grid(row=3,column=0)
 
-button6=Button(win,text='',font = ("Arial", 20, "bold"), height=3,width=12,fg='green',bg='pink',command=lambda :click(6))
-button6.grid(row=2,column=2)
+button2=Button(win,text='',font = ("Arial", 20, "bold"), height=3,width=12,bd=5,fg='green',command=lambda :click(2))
+button2.grid(row=3,column=1)
 
-button7=Button(win,text='',font = ("Arial", 20, "bold"), height=3,width=12,fg='green',bg='pink',command=lambda :click(7))   #name of figure,text on button,height=no. of lines,width=no. of char,fg=color of text,bg=background color,command=to perform an action after clicking on botton
-button7.grid(row=3,column=0)
+button3=Button(win,text='',font = ("Arial", 20, "bold"), height=3,width=12,bd=5,fg='green',command=lambda :click(3))
+button3.grid(row=3,column=2)
 
-button8=Button(win,text='',font = ("Arial", 20, "bold"), height=3,width=12,fg='green',bg='pink',command=lambda :click(8))
-button8.grid(row=3,column=1)
+button4=Button(win,text='',font = ("Arial", 20, "bold"), height=3,width=12,bd=5,fg='green',command=lambda :click(4))   #name of figure,text on button,height=no. of lines,width=no. of char,fg=color of text,bg=background color,command=to perform an action after clicking on botton
+button4.grid(row=4,columnspan=1)
 
-button9=Button(win,text='',font = ("Arial", 20, "bold"), height=3,width=12,fg='green',bg='pink',command=lambda :click(9))
-button9.grid(row=3,column=2)
+button5=Button(win,text='',font = ("Arial", 20, "bold"), height=3,width=12,bd=5,fg='green',command=lambda :click(5))
+button5.grid(row=4,column=1)
+
+button6=Button(win,text='',font = ("Arial", 20, "bold"), height=3,width=12,bd=5,fg='green',command=lambda :click(6))
+button6.grid(row=4,column=2)
+
+button7=Button(win,text='',font = ("Arial", 20, "bold"), height=3,width=12,bd=5,fg='green',command=lambda :click(7))   #name of figure,text on button,height=no. of lines,width=no. of char,fg=color of text,bg=background color,command=to perform an action after clicking on botton
+button7.grid(row=5,columnspan=1)
+
+button8=Button(win,text='',font = ("Arial", 20, "bold"), height=3,width=12,bd=5,fg='green',command=lambda :click(8))
+button8.grid(row=5,column=1)
+
+button9=Button(win,text='',font = ("Arial", 20, "bold"), height=3,width=12,bd=5,fg='green',command=lambda :click(9))
+button9.grid(row=5,column=2)
 
 win.mainloop()
